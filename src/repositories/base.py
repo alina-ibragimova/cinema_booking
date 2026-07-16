3351d943106ac9eae50fa63debee71790b31a062
Alina <alinaibragimova1526@gmail.com>
2026-07-16 22:54:38 +0300

    src/repositories

---
+++ src/repositories/base.py
@@ -0,0 +1,13 @@
+from sqlalchemy import select, update, delete, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
+
+class BaseRepository[ModelType, SchemaType: BaseModel]:
+    def __init__(self, model: type[ModelType], session: AsyncSession):
+        self.model = model
+        self.session = session
+    
+    async def get_by_id(self, id: int) -> ModelType | None:
+        query = select(self.model).where(self.model.id == id)
+        result = await self.session.execute(query)
+        return result.scalars().first()
