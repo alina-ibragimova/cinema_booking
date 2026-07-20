ee400857b75d36f0b496c417571fd468030bfb2f
Alina <alinaibragimova1526@gmail.com>
2026-07-20 23:08:03 +0300

    new booking.py

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,3 +1,4 @@
+from typing import Any, Sequence
 from sqlalchemy import select, update, delete, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import BaseModel
@@ -10,6 +11,27 @@ class BaseRepository[ModelType, SchemaType: BaseModel]:
     async def get_by_id(self, id: int) -> ModelType | None:
         query = select(self.model).where(self.model.id == id)
         result = await self.session.execute(query)
+        return result.scalar_one()
+    
+    async def get_all(self, skip: int = 0, limit: int= 100) -> Sequence[ModelType]:
+        query = select(self.model).offset(skip).limit(limit)
+        result= await self.session.execute(query)
+        return result.scalars().all()
+    
+    async def create(self, data: SchemaType) ->ModelType:
+        data_dict = data.model_dump()
+        query = insert(self.model).values(**data_dict).returning(self.model)
+        result = await self.session.execute(query)
+        await self.session.commit()
+        return result.scalar_one()
+    
+    async def update(self, id: int, data: dict[str, Any]) -> ModelType | None:
+        query = update(self.model).where(self.model.id==id).values(**data).returning(self.model)
+        result = await self.session.execute(query)
+        await self.session.commit()
         return result.scalars().first()
-    
-    
+
+    async def delete(self, id:int) ->None:
+        query = delete(self.model).where(self.model.id==id)
+        await self.session.execute(query)
+        await self.session.commit()
---
+++ src/repositories/booking.py
@@ -0,0 +1,25 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repositories.base import BaseRepository
+from src.models.booking import Booking 
+from src.schemas.booking import BookingCreate
+
+
+class BookingRepository(BaseRepository[Booking, BookingCreate]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(model=Booking, session=session)
+
+    async def get_user_bookings(self, user_id: int) -> list[Booking]:
+        query = select(self.model).where(self.model.user_id == user_id)
+        result = await self.session.execute(query)
+        return result.scalars().all()
+    
+    async def is_seat_taken(self, session_id: int, seat_number: int) -> bool:
+        query = select(self.model).where(
+            self.model.session_id == session_id,
+            self.model.seat_number == seat_number,
+            self.model.status == "active"
+        )
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none() is not None
